15e0e76b54dda34ec77f0871f4eccf5d28f0fa08
Daniel Imms <daimms@microsoft.com>
2016-04-24 18:28:36 -0700

    Add merge sort
    Fixes #11

---
+++ test/merge_sort_test.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+import sys
+
+from base_custom_comparison_sort_test import BaseCustomComparisonSortTest
+from base_positive_integer_sort_test import BasePositiveIntegerSortTest
+from base_negative_integer_sort_test import BaseNegativeIntegerSortTest
+from base_string_sort_test import BaseStringSortTest
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'sort'))
+import merge_sort
+
+class MergeSortSortTest(unittest.TestCase,
+                       BaseCustomComparisonSortTest,
+                       BasePositiveIntegerSortTest,
+                       BaseNegativeIntegerSortTest,
+                       BaseStringSortTest):
+  def setUp(self):
+    self.sort = merge_sort.sort
+
+if __name__ == '__main__':
+  unittest.main()
